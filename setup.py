8a76b2047c3ba3e1305a1d89909d208322a696c9
Kyle Gerard Felker <felker@anl.gov>
2020-08-20 12:47:58 -0500

    Slim down setup.py dependencies

--- setup.py
+++ setup.py
@@ -4,36 +4,32 @@ from setuptools import setup, find_packages
 
 import plasma.version
 
-try:
-    os.environ['MPICC'] = subprocess.check_output(
-        "which mpicc", shell=True).decode("utf-8")
-except BaseException:
-    print("Please set up the OpenMPI environment")
-    exit(1)
+# try:
+#     os.environ['MPICC'] = subprocess.check_output(
+#         "which mpicc", shell=True).decode("utf-8")
+# except BaseException:
+#     print("Please set up the OpenMPI environment")
+#     exit(1)
 
 setup(name="plasma",
       version=plasma.version.__version__,
       packages=find_packages(),
       # scripts = [""],
-      description="PPPL deep learning package.",
+      description="PyTorch models for fusion disruption prediction",
       long_description="""Add description here""",
-      author="Julian Kates-Harbeck, Alexey Svyatkovskiy",
-      author_email="jkatesharbeck@g.harvard.edu",
-      maintainer="Alexey Svyatkovskiy",
-      maintainer_email="alexeys@princeton.edu",
+      author="Ge Dong, Kyle Gerard Felker",
+      author_email="gdong@princeton.edu",
+      maintainer="Kyle Gerard Felker",
+      maintainer_email="felker@anl.gov",
       # url = "http://",
-      download_url="https://github.com/PPPLDeepLearning/plasma-python",
+      download_url="https://github.com/PPPLDeepLearning/fusiondl-torch",
       # license = "Apache Software License v2",
-      test_suite="tests",
+      # test_suite="tests",
       install_requires=[
-          'keras>=2.0.5',
           'pathos',
           'matplotlib>=2.0.2',
-          'hyperopt',
-          'mpi4py',
-          'xgboost',
-          'scikit-learn',
-          'joblib',
+          # 'scikit-learn',
+          # 'joblib',
           ],
       tests_require=[],
       classifiers=["Development Status :: 3 - Alpha",
